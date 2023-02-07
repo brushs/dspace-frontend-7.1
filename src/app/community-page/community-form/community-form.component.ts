f130c8303eb2ee074cc6aafb867c53d08f261247
stevenson.gossage <stevenson.gossage@ssc-spc.gc.ca>
2023-02-07 06:30:05 -0500

    Community create form - debugging continued

--- src/app/community-page/community-form/community-form.component.ts
+++ src/app/community-page/community-form/community-form.component.ts
@@ -57,9 +57,9 @@ export class CommunityFormComponent extends ComColFormComponent<Community> {
     new DynamicInputModel({
       id: 'title',
       name: 'dc.title',
-      labelTooltip: 'title.hint',
+/*      labelTooltip: 'title.hint',
       controlTooltip: 'title.hint',
-      required: true,
+*/      required: true,
       validators: {
         required: null
       },
@@ -74,9 +74,9 @@ export class CommunityFormComponent extends ComColFormComponent<Community> {
     new DynamicInputModel({
       id: 'titletranslation',
       name: 'dc.title.fosrctranslation',
-      labelTooltip: 'titletranslation.hint',
+/*      labelTooltip: 'titletranslation.hint',
       controlTooltip: 'titletranslation.hint',
-      hint: 'titletranslation.hint',
+*/      hint: 'titletranslation.hint',
       required: true,
       validators: {
         required: null
@@ -89,9 +89,9 @@ export class CommunityFormComponent extends ComColFormComponent<Community> {
     new DynamicInputModel({
       id: 'alternative-names',
       name: 'dc.title.alternative',
-      labelTooltip: 'alternative-names.hint',
+/*      labelTooltip: 'alternative-names.hint',
       controlTooltip: 'alternative-names.hint',
-      required: false,
+*/      required: false,
       validators: {
         required: null
       },
@@ -104,9 +104,9 @@ export class CommunityFormComponent extends ComColFormComponent<Community> {
     new DynamicInputModel({
       id: 'alternative-names-translation',
       name: 'dc.title.alternativetranslation',
-      labelTooltip: 'alternative-names-translation.hint',
+/*      labelTooltip: 'alternative-names-translation.hint',
       controlTooltip: 'alternative-names-translation.hint',
-      required: false,
+*/      required: false,
       validators: {
         required: null
       },
@@ -119,9 +119,9 @@ export class CommunityFormComponent extends ComColFormComponent<Community> {
     new DynamicInputModel({
       id: 'identifiers',
       name: 'dc.identifiers',
-      labelTooltip: 'identifiers.hint',
+/*      labelTooltip: 'identifiers.hint',
       controlTooltip: 'identifiers.hint',
-      required: false,
+*/      required: false,
       validators: {
         required: 'identifiers.required'
       },
@@ -140,9 +140,9 @@ export class CommunityFormComponent extends ComColFormComponent<Community> {
     new DynamicInputModel({
       id: 'researcharea',
       name: 'dc.subject',
-      labelTooltip: 'researcharea.hint',
+/*      labelTooltip: 'researcharea.hint',
       controlTooltip: 'researcharea.hint',
-      required: true,
+*/      required: true,
       validators: {
         required: 'researcharea.required'
       },
@@ -151,9 +151,9 @@ export class CommunityFormComponent extends ComColFormComponent<Community> {
     new DynamicInputModel({
       id: 'researcharea-fosrctranslation',
       name: 'dc.subject.fosrctranslation',
-      labelTooltip: 'researcharea-fosrctranslation.hint',
+/*      labelTooltip: 'researcharea-fosrctranslation.hint',
       controlTooltip: 'researcharea-fosrctranslation.hint',
-      required: true,
+*/      required: true,
       validators: {
         required: 'researcharea.required'
       },
@@ -163,9 +163,9 @@ export class CommunityFormComponent extends ComColFormComponent<Community> {
     new DynamicInputModel({
       id: 'home-page',
       name: 'dc.identifier.home-page',
-      labelTooltip: 'home-page.hint',
+/*      labelTooltip: 'home-page.hint',
       controlTooltip: 'home-page.hint',
-      required: false,
+*/      required: false,
       validators: {
         required: 'home-page.required'
       },
@@ -174,9 +174,9 @@ export class CommunityFormComponent extends ComColFormComponent<Community> {
     new DynamicInputModel({
       id: 'home-pagetranslation',
       name: 'dc.identifier.home-pagetranslation',
-      labelTooltip: 'home-pagetranslation.hint',
+/*      labelTooltip: 'home-pagetranslation.hint',
       controlTooltip: 'home-pagetranslation.hint',
-      required: false,
+*/      required: false,
       validators: {
         required: 'home-pagetranslation.required'
       },
@@ -186,9 +186,9 @@ export class CommunityFormComponent extends ComColFormComponent<Community> {
     new DynamicInputModel({
       id: 'description-email-address',
       name: 'dc.description.email-address',
-      labelTooltip:   'description-email-address.hint',
+/*      labelTooltip:   'description-email-address.hint',
       controlTooltip: 'description-email-address.hint',
-      hint:           'description-email-address.hint',
+*/      hint:           'description-email-address.hint',
       required: false,
       validators: {
         required:     'description-email-address.required'
@@ -199,9 +199,9 @@ export class CommunityFormComponent extends ComColFormComponent<Community> {
     new DynamicTextAreaModel({
       id: 'abstract',
       name: 'dc.description.abstract',
-      labelTooltip:   'abstract.hint',
+/*      labelTooltip:   'abstract.hint',
       controlTooltip: 'abstract.hint',
-      hint:           'abstract.hint',
+*/      hint:           'abstract.hint',
       required: false,
       validators: {
         required:     'abstract.required'
@@ -210,9 +210,9 @@ export class CommunityFormComponent extends ComColFormComponent<Community> {
     new DynamicTextAreaModel({
       id: 'abstracttranslation',
       name: 'dc.description.abstracttranslation',
-      labelTooltip:   'abstracttranslation.hint',
+/*      labelTooltip:   'abstracttranslation.hint',
       controlTooltip: 'abstracttranslation.hint',
-      hint:           'abstracttranslation.hint',
+*/      hint:           'abstracttranslation.hint',
       required: false,
       validators: {
         required:     'abstracttranslation.required'
@@ -222,9 +222,9 @@ export class CommunityFormComponent extends ComColFormComponent<Community> {
     new DynamicTextAreaModel({
       id: 'description',
       name: 'dc.description',
-      labelTooltip:   'description.hint',
+/*      labelTooltip:   'description.hint',
       controlTooltip: 'description.hint',
-      hint:           'description.hint',
+*/      hint:           'description.hint',
       required: false,
       validators: {
         required:     'description.required'
@@ -233,9 +233,9 @@ export class CommunityFormComponent extends ComColFormComponent<Community> {
     new DynamicTextAreaModel({
       id: 'description-fosrctranslation',
       name: 'dc.description.fosrctranslation',
-      labelTooltip:   'description-fosrctranslation.hint',
+/*      labelTooltip:   'description-fosrctranslation.hint',
       controlTooltip: 'description-fosrctranslation.hint',
-      hint:           'description-fosrctranslation.hint',
+*/      hint:           'description-fosrctranslation.hint',
       required: false,
       validators: {
         required:     'description-fosrctranslation.required'
@@ -244,9 +244,9 @@ export class CommunityFormComponent extends ComColFormComponent<Community> {
     new DynamicTextAreaModel({
       id: 'description-contact-address',
       name: 'dc.description.contact-address',
-      labelTooltip:   'description-contact-address.hint',
+/*      labelTooltip:   'description-contact-address.hint',
       controlTooltip: 'description-contact-address.hint',
-      hint:           'description-contact-address.hint',
+*/      hint:           'description-contact-address.hint',
       required: false,
       validators: {
         required:     'description-contact-address.required'
@@ -256,9 +256,9 @@ export class CommunityFormComponent extends ComColFormComponent<Community> {
     new DynamicTextAreaModel({
       id: 'tableofcontents',
       name: 'dc.description.tableofcontents',
-      labelTooltip:   'tableofcontents.hint',
+/*      labelTooltip:   'tableofcontents.hint',
       controlTooltip: 'tableofcontents.hint',
-      hint:           'tableofcontents.hint',
+*/      hint:           'tableofcontents.hint',
       required: false,
       validators: {
         required:     'tableofcontents.required'
@@ -267,9 +267,9 @@ export class CommunityFormComponent extends ComColFormComponent<Community> {
     new DynamicTextAreaModel({
       id: 'tableofcontentsTranslation',
       name: 'dc.description.tableofcontentsTranslation',
-      labelTooltip:   'tableofcontentsTranslation.hint',
+/*      labelTooltip:   'tableofcontentsTranslation.hint',
       controlTooltip: 'tableofcontentsTranslation.hint',
-      hint:           'tableofcontentsTranslation.hint',
+  */    hint:           'tableofcontentsTranslation.hint',
       required: false,
       validators: {
         required:     'tableofcontentsTranslation.required'
@@ -279,9 +279,9 @@ export class CommunityFormComponent extends ComColFormComponent<Community> {
     new DynamicTextAreaModel({
       id: 'rights',
       name: 'dc.rights',
-      labelTooltip:   'rights.hint',
+/*      labelTooltip:   'rights.hint',
       controlTooltip: 'rights.hint',
-      hint:           'rights.hint',
+*/      hint:           'rights.hint',
       required: false,
       validators: {
         required:     'rights.required'
@@ -290,14 +290,14 @@ export class CommunityFormComponent extends ComColFormComponent<Community> {
     new DynamicTextAreaModel({
       id: 'rights-fosrctranslation',
       name: 'dc.rights.fosrctranslation',
-      labelTooltip:   'rights-fosrctranslation.hint',
+/*      labelTooltip:   'rights-fosrctranslation.hint',
       controlTooltip: 'rights-fosrctranslation.hint',
-      hint:           'rights-fosrctranslation.hint',
+*/      hint:           'rights-fosrctranslation.hint',
       required: false,
       validators: {
         required:     'rights-fosrctranslation.required'
       }
-    }),
+    })
 
     /* OSPR changes end - add/remove fields for testing the new dynamic control models */
   ];
