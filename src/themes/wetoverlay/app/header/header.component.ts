cc95d7c84f684ef58d0151b5c9b733b9a9d4109a
Ikram Mohamed Navaz <ikrammohamed5@gmail.com>
2023-03-07 13:53:36 -0500

    added screen check for removing button when not visible

--- src/themes/wetoverlay/app/header/header.component.ts
+++ src/themes/wetoverlay/app/header/header.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { HeaderComponent as BaseComponent } from '../../../../app/header/header.component';
 
+const TOGGLE_DISPLAY_BREAKPOINT_SIZE = 768;
 /**
  * Represents the header with the logo and simple navigation
  */
@@ -11,5 +12,16 @@ import { HeaderComponent as BaseComponent } from '../../../../app/header/header.
   templateUrl: 'header.component.html',
   // templateUrl: '../../../../app/header/header.component.html',
 })
-export class HeaderComponent extends BaseComponent {
+export class HeaderComponent extends BaseComponent implements OnInit{
+  hideToggleButton; // Remove button component when not supposed to be visible for accessibility compliance
+
+  ngOnInit(): void {
+    this.hideToggleButton = (window.innerWidth > TOGGLE_DISPLAY_BREAKPOINT_SIZE);
+  }
+
+  @HostListener("window:resize", [])
+  onResize() {
+    console.log('here', window.innerWidth)
+    this.hideToggleButton = !!(window.innerWidth > TOGGLE_DISPLAY_BREAKPOINT_SIZE);
+  }
 }
