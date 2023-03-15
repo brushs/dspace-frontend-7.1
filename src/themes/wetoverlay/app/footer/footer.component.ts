0786a48c66612ad82c340bc8c07882d81a90b13b
Ikram Mohamed Navaz <ikrammohamed5@gmail.com>
2023-03-15 17:05:51 -0400

    added gcweb files + changes to hear + footer

--- src/themes/wetoverlay/app/footer/footer.component.ts
+++ src/themes/wetoverlay/app/footer/footer.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { FooterComponent as BaseComponent } from '../../../../app/footer/footer.component';
 
 @Component({
   selector: 'ds-footer',
-  styleUrls: ['footer.component.scss'],
+  styleUrls: ['footer.component.scss', '../../styles/wet-theme.scss'],
   //styleUrls: ['../../../../app/footer/footer.component.scss'],
   templateUrl: 'footer.component.html'
   //templateUrl: '../../../../app/footer/footer.component.html'
 })
 export class FooterComponent extends BaseComponent {
+
+  // ngAfterViewInit(): void {
+  //   import('../../scripts/jquery.magnific-popup.min.js');
+  //    import('../../scripts/wet-boew.min.js');
+  //    import('../../scripts/theme.min.js');
+  // }
 }
--- src/themes/wetoverlay/app/header/header.component.ts
+++ src/themes/wetoverlay/app/header/header.component.ts
@@ -8,18 +8,33 @@ import { HeaderComponent as BaseComponent } from '../../../../app/header/header.
  */
 @Component({
   selector: 'ds-header',
-  styleUrls: ['header.component.scss'],
+  styleUrls: ['../../styles/wet-theme.scss'],
   // styleUrls: ['../../../../app/header/header.component.scss'],
   templateUrl: 'header.component.html',
   // templateUrl: '../../../../app/header/header.component.html',
 })
 export class HeaderComponent extends BaseComponent {
-  isXsOrSm$; // Remove button component when not supposed to be visible for accessibility compliance
+  menuItems: Array<{link: string, label: string}> = [
+    {link: 'https://www.canada.ca/en/services/jobs.html', label: 'Jobs and the workplace'},
+    {link: 'https://www.canada.ca/en/services/immigration-citizenship.html', label: 'Immigration and citizenship'},
+    {link: 'https://travel.gc.ca/', label: 'Travel and tourism'},
+    {link: 'https://www.canada.ca/en/services/business.html', label: 'Business and industry'},
+    {link: 'https://www.canada.ca/en/services/environment.html', label: 'Environment and natural resources'},
+    {link: 'https://www.canada.ca/en/services/benefits.html', label: 'Benefits'},
+    {link: 'https://www.canada.ca/en/services/health.html', label: 'Health'},
+    {link: 'https://www.canada.ca/en/services/taxes.html', label: 'Taxes'},
+    {link: 'https://www.canada.ca/en/services/defence.html', label: 'National security and defence'},
+    {link: 'https://www.canada.ca/en/services/culture.html', label: 'Culture, history and sport'},
+    {link: 'https://www.canada.ca/en/services/policing.html', label: 'Policing, justice and emergencies'},
+    {link: 'https://www.canada.ca/en/services/transport.html', label: 'Transport and infrastructure'},
+    {link: 'https://international.gc.ca/world-monde/index.aspx?lang=eng', label: 'Canada and the world'},
+    {link: 'https://www.canada.ca/en/services/finance.html', label: 'Money and finances'},
+    {link: 'https://www.canada.ca/en/services/science.html', label: 'Science and innovation'},
+  ]
   constructor(
     protected windowService: HostWindowService,
     menuService: MenuService
   ) {
     super(menuService);
-    this.isXsOrSm$ = this.windowService.isXsOrSm();
   }
 }
