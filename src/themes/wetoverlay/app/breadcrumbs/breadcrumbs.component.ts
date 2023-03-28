2d9ff7f9eb6502f598f1ebfe3f151411261ec7fb
Ikram Mohamed Navaz <ikrammohamed5@gmail.com>
2023-03-28 12:15:51 -0400

    added gcweb style breadcrumbs

--- src/themes/wetoverlay/app/breadcrumbs/breadcrumbs.component.ts
+++ src/themes/wetoverlay/app/breadcrumbs/breadcrumbs.component.ts
@@ -8,7 +8,7 @@ import { BreadcrumbsComponent as BaseComponent } from '../../../../app/breadcrum
   selector: 'ds-breadcrumbs',
   templateUrl: './breadcrumbs.component.html',
   //templateUrl: '../../../../app/breadcrumbs/breadcrumbs.component.html',
-  styleUrls: ['./breadcrumbs.component.scss']
+  styleUrls: ['../../styles/wet-theme.scss']
   //styleUrls: ['../../../../app/breadcrumbs/breadcrumbs.component.scss']
 })
 export class BreadcrumbsComponent extends BaseComponent {
