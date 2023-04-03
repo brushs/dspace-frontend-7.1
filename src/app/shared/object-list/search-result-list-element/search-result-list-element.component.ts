e4852f1a1e4f51c1715e24c1dedd314e6a7003c5
Mohammad Raza <mraza@live.ca>
2023-04-03 17:08:51 -0400

    updated single item page

--- src/app/shared/object-list/search-result-list-element/search-result-list-element.component.ts
+++ src/app/shared/object-list/search-result-list-element/search-result-list-element.component.ts
@@ -35,7 +35,7 @@ export class SearchResultListElementComponent<T extends SearchResult<K>, K exten
     if (hasValue(this.object)) {
       this.dso = this.object.indexableObject;
       this.dsoTitle = this.dsoNameService.getName(this.dso);
-      this.dsoOfficialTitle = this.dsoNameService.getOfficialName(this.dso); //FOSRC added
+      this.dsoOfficialTitle = this.dsoNameService.getOfficialName(this.dso, this.localeService.getCurrentLanguageCode() === 'fr' ? 'fr' : 'en'); //FOSRC added
       this.dsoTranslatedTitle = this.dsoNameService.getTranslatedName(this.dso, this.localeService.getCurrentLanguageCode() === 'fr' ? 'fr' : 'en'); //FOSRC added
       console.log("ngOnInit: " + this.dsoOfficialTitle);
     }
