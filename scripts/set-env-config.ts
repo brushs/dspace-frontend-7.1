26f84698e3738f24f58f409633205520a334c042
steve.brush@apption.com <steve.brush@apption.com>
2023-04-21 15:54:09 -0400

    Env config update

--- scripts/set-env-config.ts
+++
@@ -1,5 +0,0 @@
-import { AppConfig } from '../src/config/app-config.interface';
-import { buildAppConfig } from '../src/config/config.server';
-import { join } from 'path';
-
-const appConfig: AppConfig = buildAppConfig(join(process.cwd(), 'src/assets/config.json'));
--- src/config/item-config.interface.ts
+++
@@ -1,9 +0,0 @@
-import { Config } from './config.interface';
-
-export interface ItemConfig extends Config {
-  edit: {
-    undoTimeout: number;
-  };
-  // This is used to show the access status label of items in results lists
-  showAccessStatuses: boolean;
-}
