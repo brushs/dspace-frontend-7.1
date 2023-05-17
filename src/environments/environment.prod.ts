c1d37cecf00fa1a25da3fbe8f2fa9013ce19d5e4
steve.brush@apption.com <steve.brush@apption.com>
2023-05-17 09:03:09 -0400

    Prod config updates

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,47 @@
+import { GlobalConfig } from '../config/global-config.interface';
+
+export const environment: Partial<GlobalConfig> = {
+  production: true,
+  // Angular Universal server settings.
+  // NOTE: these must be "synced" with the 'dspace.ui.url' setting in your backend's local.cfg.
+  ui: {
+    ssl: true,
+    host: '0.0.0.0',
+    port: 4000,
+    // NOTE: Space is capitalized because 'namespace' is a reserved string in TypeScript
+    nameSpace: '/',
+    // The rateLimiter settings limit each IP to a "max" of 500 requests per "windowMs" (1 minute).
+    rateLimiter: {
+      windowMs: 1 * 60 * 1000,   // 1 minute
+      max: 500 // limit each IP to 500 requests per windowMs
+    }
+  },
+  // The REST API server settings.
+  // NOTE: these must be "synced" with the 'dspace.server.url' setting in your backend's local.cfg.
+  rest: {
+    ssl: true,
+    host: 'dspacesandboxbackend.azurewebsites.net',
+    port: null,
+    // NOTE: Space is capitalized because 'namespace' is a reserved string in TypeScript
+    nameSpace: '/server',
+  },
+  auth: {
+    // Authentication UI settings
+    ui: {
+      // the amount of time before the idle warning is shown
+      timeUntilIdle: 15 * 60 * 1000, // 15 minutes
+      // the amount of time the user has to react after the idle warning is shown before they are logged out.
+      idleGracePeriod: 5 * 60 * 1000, // 5 minutes
+    },
+    // Authentication REST settings
+    rest: {
+      // If the rest token expires in less than this amount of time, it will be refreshed automatically.
+      // This is independent from the idle warning.
+      timeLeftBeforeTokenRefresh: 2 * 60 * 1000, // 2 minutes
+    },
+    login: {
+      // Whether or not to show the password field on the login page.
+      enablePassword: false
+    }
+  }
+}
