a45123df1bcbb251ad0c285a1976b7403cef5132
Ikram Mohamed Navaz <ikrammohamed5@gmail.com>
2023-05-26 11:49:55 -0400

    added global footer

--- src/themes/wetoverlay/app/footer/footer.component.ts
+++ src/themes/wetoverlay/app/footer/footer.component.ts
@@ -1,5 +1,7 @@
 import { AfterViewInit, Component } from '@angular/core';
 import { FooterComponent as BaseComponent } from '../../../../app/footer/footer.component';
+import { TranslateService } from '@ngx-translate/core';
+import { AppInjector } from '../../../../app/app.injector';
 
 @Component({
   selector: 'ds-footer',
@@ -9,5 +11,8 @@ import { FooterComponent as BaseComponent } from '../../../../app/footer/footer.
   //templateUrl: '../../../../app/footer/footer.component.html'
 })
 export class FooterComponent extends BaseComponent {
-  
+  translate: TranslateService
+  ngOnInit() {
+    this.translate = AppInjector.get(TranslateService);
+  }
 }
