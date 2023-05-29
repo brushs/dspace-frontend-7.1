153c73511504a040158ecfc37ad59fccfab85860
Ikram Mohamed Navaz <ikrammohamed5@gmail.com>
2023-05-29 14:35:33 -0400

    global header change

--- src/themes/wetoverlay/app/breadcrumbs/breadcrumbs.component.ts
+++ src/themes/wetoverlay/app/breadcrumbs/breadcrumbs.component.ts
@@ -5,7 +5,7 @@ import { BreadcrumbsComponent as BaseComponent } from '../../../../app/breadcrum
  * Component representing the breadcrumbs of a page
  */
 @Component({
-  selector: 'ds-breadcrumbs',
+  selector: '[ds-breadcrumbs]',
   templateUrl: './breadcrumbs.component.html',
   //templateUrl: '../../../../app/breadcrumbs/breadcrumbs.component.html',
   styleUrls: ['../../styles/wet-theme.scss']
