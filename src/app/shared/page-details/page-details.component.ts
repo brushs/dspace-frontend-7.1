2f0a9f1b690166003e81def0c6c9e9d0b5675d9e
Ikram Mohamed Navaz <ikrammohamed5@gmail.com>
2023-05-31 14:11:08 -0400

    added page details

---
+++ src/app/shared/page-details/page-details.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'ds-page-details, [ds-page-details]',
+  styleUrls: ['./page-details.component.scss'],
+  templateUrl: './page-details.component.html'
+})
+export class PageDetailsComponent {
+
+}
