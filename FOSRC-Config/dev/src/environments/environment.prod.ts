f6ea90d5c28dc4588531968c0cbbe4bddee40972
Stevenson Gossage <stevenson.gossage@ssc-spc.gc.ca>
2023-06-16 14:26:30 +0000

    Adding fronetend config for original and new DEV environments

---
+++ FOSRC-Config/dev/src/environments/environment.prod.ts
@@ -0,0 +1,18 @@
+
+
+export const environment = {
+  production: true,
+  rest: {
+	  ssl: true,	 
+	  host: 'dev-ospr.g.ent.cloud-nuage.canada.ca', 
+	  port: 443,
+	  nameSpace: '/server',
+  },
+  themes: [{
+	name: 'wetoverlay'
+  }],
+   mediaViewer: {
+	 image: false,
+	video: false,
+   }
+};
