b27c52b09a51500496af852ee07e17cb8b88009c
Ikram Mohamed Navaz <ikrammohamed5@gmail.com>
2023-07-11 16:54:42 -0400

    share button refresh fix

--- src/app/shared/page-details/page-details.component.ts
+++ src/app/shared/page-details/page-details.component.ts
@@ -1,17 +1,35 @@
 
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Renderer2, Inject, ViewChild } from '@angular/core';
+import { NavigationEnd, NavigationStart, Router,  } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
+import { filter } from 'rxjs/operators';
+import { initShareBtn } from './initShareButton';
 
 @Component({
   selector: 'ds-page-details, [ds-page-details]',
   styleUrls: ['./page-details.component.scss'],
   templateUrl: './page-details.component.html'
 })
+
+
 export class PageDetailsComponent {
   lastModified;
-  constructor(public translate: TranslateService) {}
+
+
+  constructor(public translate: TranslateService,
+    ) {
+    }
 
   ngOnInit(){
     this.lastModified = document.lastModified;
   }
+
+  ngAfterViewInit() {
+    // Reinitialize the share button every time a refresh occurs. This prevents the share button from disappearing.
+   if(globalThis.wb && globalThis.jQuery) {
+      initShareBtn(globalThis.jQuery, window, document, globalThis.wb);
+    }
+  }
+
 }
