32e8fd46b3b64c2eff1b29e13e5ce55a278dcb86
Nick Salt <nsalt@apption.com>
2023-08-17 13:51:57 -0400

    refactored doi

--- src/app/item-page/simple/field-components/specific-field/uri/item-page-uri-field.component.ts
+++ src/app/item-page/simple/field-components/specific-field/uri/item-page-uri-field.component.ts
@@ -33,5 +33,21 @@ export class ItemPageUriFieldComponent extends ItemPageFieldComponent {
    * Label i18n key for the rendered metadata
    */
   @Input() label: string;
+
+  @Input() isDescriptionList: boolean = false;
   
+  public domainAwareValue (rawValue: string): string {
+    const domainList = ['dev.ospr.link','ospr.link','localhost','open-science.canada.ca','science-ouverte.canada.ca', 'ospr.g.ent.cloud-nauge.canada.ca']
+    if(!domainList.some((x: string) => rawValue.includes(x))) {
+      return rawValue;
+    }
+    //console.log("outside domainAware if statement: " + rawValue)
+    try {
+      const urlObj = new URL(rawValue);
+      const currentHostname = location.hostname;
+      return rawValue.replace(urlObj.hostname, currentHostname);
+    } catch (Error) {
+      return rawValue;
+    }
+  }
 }
