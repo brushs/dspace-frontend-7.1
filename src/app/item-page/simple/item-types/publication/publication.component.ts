6864b91a2b45a0746043b2cbc641cb840c012b74
jjcc <jjccforth@gmail.com>
2023-08-27 16:32:14 -0400

    collection related

--- src/app/item-page/simple/item-types/publication/publication.component.ts
+++ src/app/item-page/simple/item-types/publication/publication.component.ts
@@ -2,6 +2,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ItemComponent } from '../shared/item.component';
 import { ViewMode } from '../../../../core/shared/view-mode.model';
 import { listableObjectComponent } from '../../../../shared/object-collection/shared/listable-object/listable-object.decorator';
+import { Subscription } from 'rxjs';
 
 /**
  * Component that represents a publication Item page
@@ -15,5 +16,20 @@ import { listableObjectComponent } from '../../../../shared/object-collection/sh
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PublicationComponent extends ItemComponent {
+
+  private myObservableSubscription: Subscription;
+  public ownCollection: any;
+  ngOnInit(): void {
+    const collection = this.object.owningCollection;
+    super.ngOnInit();
+    this.myObservableSubscription = collection.subscribe(
+      (value) => {
+        console.log('Value received from the Observable:', value);
+        if (value?.isSuccess)
+          this.ownCollection = value.payload;
+      }
+    );
+    console.log('PublicationComponent ngOnInit()');
+  }
 
 }
