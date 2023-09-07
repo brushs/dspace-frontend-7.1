8badf58e1207f9e7bfa3abb6efb604b0963ca988
Ikram Mohamed Navaz <mohamednavazik@csc-scc.gc.ca>
2023-09-07 15:02:02 -0400

    page content addition

--- src/app/shared/comcol-page-header/comcol-page-header.component.ts
+++ src/app/shared/comcol-page-header/comcol-page-header.component.ts
@@ -2,7 +2,7 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'ds-comcol-page-header',
-  styleUrls: ['./comcol-page-header.component.scss'],
+  styleUrls: ['./comcol-page-header.component.scss', '../../../themes/wetoverlay/styles/static-pages.scss'],
   templateUrl: './comcol-page-header.component.html',
 })
 export class ComcolPageHeaderComponent {
