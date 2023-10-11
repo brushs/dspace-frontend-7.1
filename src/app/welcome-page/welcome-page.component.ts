fa90c0993333f4778c61141ab5d9450eee9dc33b
Eric Ho <ericho613@gmail.com>
2023-10-11 10:43:54 -0400

    1892-Modified welcome-page component TS file.  Added method to load link elements programmatically.

--- src/app/welcome-page/welcome-page.component.ts
+++ src/app/welcome-page/welcome-page.component.ts
@@ -40,6 +40,9 @@ export class WelcomePageComponent implements OnInit {
 
   ngOnInit(): void {
 
+    //load the links
+    this.loadLinks();
+
     //start loading the scripts sequentially
     this.loadNextScript();
 
@@ -83,6 +86,42 @@ export class WelcomePageComponent implements OnInit {
         console.error(this.scriptsToLoad[this.scriptsLoadedCount], 'failed', error);
           this.scriptsLoadedCount++;
       })
+
+  }
+
+  loadLinks(){
+    const link1 = this.renderer2.createElement('link');
+    link1.rel = "apple-touch-icon";
+    link1.sizes = "57x57 72x72 114x114 144x144 150x150";
+    link1.class = "wb-favicon";
+    link1.href = "assets/gcweb/assets/favicon-mobile.png";
+    this.renderer2.appendChild(this.document.head, link1);
+      
+    const link2 = this.renderer2.createElement('link');
+    link2.rel = "icon";
+    link2.type = "image/x-icon";
+    link2.class = "wb-init wb-favicon-inited";
+    link2.href = "assets/gcweb/assets/favicon.ico";
+    this.renderer2.appendChild(this.document.head, link2);
+
+    const link3 = this.renderer2.createElement('link');
+    link3.rel = "stylesheet";
+    link3.href = "assets/gcweb/css/theme.min.css";
+    this.renderer2.appendChild(this.document.head, link3);
+
+    const link4 = this.renderer2.createElement('link');
+    link4.rel = "stylesheet";
+    link4.href = "assets/gcweb/css/messages.min.css";
+    this.renderer2.appendChild(this.document.head, link4);
+
+    const noScript = this.renderer2.createElement('noscript');
+    const link5 = this.renderer2.createElement('link');
+    link5.rel = "stylesheet";
+    link5.href = "assets/wet-boew/css/noscript.min.css";
+    this.renderer2.appendChild(this.document.head, noScript);
+    this.renderer2.appendChild(noScript, link5);
+
+    console.log('All links loaded.');
 
   }
 
