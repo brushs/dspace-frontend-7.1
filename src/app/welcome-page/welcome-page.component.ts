463cda839aa53fafaf46efbd408c919cb37bac45
Eric Ho <ericho613@gmail.com>
2023-10-11 10:01:16 -0400

    1892-Modified welcome page component.  Removed integrity attribute addition to script.

--- src/app/welcome-page/welcome-page.component.ts
+++ src/app/welcome-page/welcome-page.component.ts
@@ -68,7 +68,6 @@ export class WelcomePageComponent implements OnInit {
       script.async = true;
       script.src = this.scriptsToLoad[this.scriptsLoadedCount];
       if(this.scriptsToLoad[this.scriptsLoadedCount] === "assets/wet-boew/js/jquery/2.2.4/jquery.min.js"){
-        script.integrity = "sha384-rY/jv8mMhqDabXSo+UCggqKtdmBfd3qC2/KvyTDNQ6PcUJXaxK1tMepoQda4g5vB";
         script.crossOrigin = "anonymous";
       };
       this.renderer2.appendChild(this.document.body, script);
