024cb463c8d2d614cb95ec161e61f111276037cb
Stevenson Gossage <stevenson.gossage@ssc-spc.gc.ca>
2023-11-28 20:54:00 +0000

    changing ui setting dev-ospr.g.ent.cloud-nuage.canada.ca from '0.0.0.0'

--- FOSRC-Config/dev/src/environments/environment.prod.ts
+++ FOSRC-Config/dev/src/environments/environment.prod.ts
@@ -3,8 +3,8 @@
 export const environment = {
   production: true,
   ui: {
-    ssl: false,
-    host: '0.0.0.0', //'10.153.200.0', //'ospr.g.ent.cloud-nuage.canada.ca',//'10.153.200.0', //'localhost',
+    ssl: true,
+    host: 'dev-ospr.g.ent.cloud-nuage.canada.ca', //'0.0.0.0', //'10.153.200.0', //'ospr.g.ent.cloud-nuage.canada.ca',//'10.153.200.0', //'localhost',
     port: 4000,
     // NOTE: Space is capitalized because 'namespace' is a reserved string in TypeScript
     nameSpace: '/',
