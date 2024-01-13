ccad1d5ea10fa23b908b9f187040cdef63ef978e
jjcc <jjccforth@gmail.com>
2024-01-13 16:34:51 -0500

    build error fix

--- src/app/core/services/translation-json.service.ts
+++ src/app/core/services/translation-json.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import JSON5 from 'json5';
+import * as JSON5 from 'json5';
 
 @Injectable({
   providedIn: 'root',
@@ -12,7 +12,7 @@ export class TranslationJsonService  {
 
   loadJson5File(lang: string): Promise<void> {
     const filePath = `assets/i18n/core/${lang}.json5`;
-    
+
     return this.http.get(filePath, { responseType: 'text' })
       .toPromise()
       .then(json5Content => {
@@ -33,51 +33,51 @@ export class TranslationJsonService  {
 
   getValueByKey<T>(key: string, lang: string): T | undefined {
     let parsedData: any;
-  
+
     // Choose the parsed data based on the language
     if (lang === 'en') {
       parsedData = this.parsedDataEnglish;
     } else if (lang === 'fr') {
       parsedData = this.parsedDataFrench;
     }
-  
+
     return parsedData && parsedData[key];
   }
 
   getKeyByValue<T>(value: T, lang: string): string | undefined {
     let parsedData: any;
-  
+
     if (lang === 'en') {
       parsedData = this.parsedDataEnglish;
     } else if (lang === 'fr') {
       parsedData = this.parsedDataFrench;
     }
-  
+
     if (parsedData) {
       const entry = Object.entries(parsedData).find(([key, val]) => val === value);
       return entry ? entry[0] : undefined;
     }
-  
+
     return undefined;
   }
 
   getKeysByValue<T>(value: T, lang: string): string[] | undefined {
     let parsedData: any;
-  
+
     if (lang === 'en') {
       parsedData = this.parsedDataEnglish;
     } else if (lang === 'fr') {
       parsedData = this.parsedDataFrench;
     }
-  
+
     if (parsedData) {
       return Object.entries(parsedData).filter(([key, val]) => val === value)
         .map(([key, val]) => {
           return key;
         });
     }
-  
+
     return undefined;
   }
-  
+
 }
--- src/ngx-translate-loaders/translate-json5-http.loader.ts
+++ src/ngx-translate-loaders/translate-json5-http.loader.ts
@@ -3,7 +3,7 @@ import { TranslateLoader } from '@ngx-translate/core';
 import { map } from 'rxjs/operators';
 import { Observable, zip } from 'rxjs';
 
-import JSON5 from 'json5';
+import * as JSON5 from 'json5';
 
 export class TranslateJson5HttpLoader implements TranslateLoader {
 
@@ -11,8 +11,8 @@ export class TranslateJson5HttpLoader implements TranslateLoader {
   }
 
   getTranslation(lang: string): Observable<any> {
-    return zip( 
-        ...this.resources.map( ({prefix, suffix}) => 
+    return zip(
+        ...this.resources.map( ({prefix, suffix}) =>
           this.http.get('' + prefix + lang + suffix, {responseType: 'text'})
             .pipe(map((json: any) => JSON5.parse(json))))
       ).pipe(map((x)=> x.reduce((prev, curr) => {
