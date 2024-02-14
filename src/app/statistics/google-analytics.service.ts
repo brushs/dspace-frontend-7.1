d92fe170b6bda9c51c77d77de7c664d68475a5b4
jjcc <jjccforth@gmail.com>
2024-02-14 09:35:43 -0500

    bug fix for google analytics

--- src/app/statistics/google-analytics.service.ts
+++ src/app/statistics/google-analytics.service.ts
@@ -77,22 +77,22 @@ export class GoogleAnalyticsService {
       const keyScriptHead = this.document.createElement('script');
       keyScriptHead.innerHTML = `
         <!-- Google Tag Manager -->
-        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','${trackingId}');</script>
+        })(window,document,'script','dataLayer','${trackingId}');
         <!-- End Google Tag Manager -->
         `;
 
       this.document.head.appendChild(keyScriptHead);
 
       // add trackingId snippet to body
-      const keyScript = this.document.createElement('script');
+      const keyScript = this.document.createElement('noscript');
       keyScript.innerHTML =   `
         <!-- Google Tag Manager (noscript) -->
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=${trackingId}"
-        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <iframe src="https://www.googletagmanager.com/ns.html?id=${trackingId}"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe>
         <!-- End Google Tag Manager (noscript) --> `;
       this.document.body.appendChild(keyScript);
   }
