53c94fdcaae6c7df66ce8c9fc02cca1f34fe4af4
jjcc <jjccforth@gmail.com>
2024-06-24 07:12:15 -0400

    show alternative download at right side

--- src/app/item-page/simple/field-components/specific-field/label-link/item-page-labellink-field.component.ts
+++ src/app/item-page/simple/field-components/specific-field/label-link/item-page-labellink-field.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output } from '@angular/core';
+import { Component, Input, Output,EventEmitter } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { Item } from 'src/app/core/shared/item.model';
 import { ItemPageFieldComponent } from '../item-page-field.component';
@@ -19,7 +19,9 @@ export class ItemPageLabelLinkComponent extends ItemPageFieldComponent {
    */
   @Input() item: Item;
   @Input() field: string;
-
+  @Input() conditional:boolean = false;
+  // Output property to notify parent component
+  @Output() shouldHide: EventEmitter<boolean> = new EventEmitter<boolean>();
 
 
 
@@ -62,6 +64,16 @@ export class ItemPageLabelLinkComponent extends ItemPageFieldComponent {
       if (link) {
         //push the link and the label to the values array
         this.values.push([link[1], link[2]]);
+        if (this.conditional)
+          // check if the value contains a special link "geoscan.nrcan.gc.ca" or  the text contains a
+          // special text "Download - Télécharger" to hide the field
+          if (link[1].includes('geoscan.nrcan.gc.ca') || link[2].includes('Download - Télécharger')) {
+            this.shouldHide.emit(true);
+            this.isHidden = false;
+          }else{
+            this.shouldHide.emit(false);
+            this.isHidden = true;
+          }
         return;
       }
       else {
