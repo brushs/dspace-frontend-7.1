eccd443fe7bd525696c46f249f7c5618cc651427
yueshao-nrcan <yue.shao@nrcan-rncan.gc.ca>
2024-07-10 12:48:03 -0400

    issue 348: Add ORCIDs to the Author pop-up window.

--- src/app/entity-groups/research-entities/item-list-elements/search-result-list-elements/person/person-search-result-list-element.component.ts
+++ src/app/entity-groups/research-entities/item-list-elements/search-result-list-elements/person/person-search-result-list-element.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { listableObjectComponent } from '../../../../../shared/object-collection/shared/listable-object/listable-object.decorator';
 import { ViewMode } from '../../../../../core/shared/view-mode.model';
 import { ItemSearchResultListElementComponent } from '../../../../../shared/object-list/search-result-list-element/item-search-result/item-types/item/item-search-result-list-element.component';
+import { uniqueId } from 'lodash';
 
 @listableObjectComponent('PersonSearchResult', ViewMode.ListElement)
 @Component({
@@ -15,8 +16,12 @@ import { ItemSearchResultListElementComponent } from '../../../../../shared/obje
 export class PersonSearchResultListElementComponent extends ItemSearchResultListElementComponent {
 
   get name() {
-    return this.value ?
-      this.value :
-      this.firstMetadataValue('person.familyName') + ', ' + this.firstMetadataValue('person.givenName');
+    // issue 348 start
+    if (this.firstMetadataValue('person.identifier.orcid')){
+      return this.firstMetadataValue('person.familyName') + ', ' + this.firstMetadataValue('person.givenName') + ' - ' + this.firstMetadataValue('person.identifier.orcid');
+    } else {
+      return this.firstMetadataValue('person.familyName') + ', ' + this.firstMetadataValue('person.givenName');
+    }
+    // issue 348 end
   }
 }
