c3771a6eab083bc153310e0af4d644b2c89cbaad
jjcc <jjccforth@gmail.com>
2024-08-17 21:21:38 -0400

    set title of the auther to it's full name

--- src/app/entity-groups/research-entities/item-pages/person/person.component.ts
+++ src/app/entity-groups/research-entities/item-pages/person/person.component.ts
@@ -2,6 +2,11 @@ import { Component } from '@angular/core';
 import { ItemComponent } from '../../../../item-page/simple/item-types/shared/item.component';
 import { ViewMode } from '../../../../core/shared/view-mode.model';
 import { listableObjectComponent } from '../../../../shared/object-collection/shared/listable-object/listable-object.decorator';
+import { Title } from '@angular/platform-browser';
+import { DSONameService } from '../../../../core/breadcrumbs/dso-name.service';
+import { LocaleService } from '../../../../core/locale/locale.service';
+import { RouteService } from '../../../../core/services/route.service';
+import { Router } from '@angular/router';
 
 @listableObjectComponent('Person', ViewMode.StandalonePage)
 @Component({
@@ -13,4 +18,20 @@ import { listableObjectComponent } from '../../../../shared/object-collection/sh
  * The component for displaying metadata and relations of an item of the type Person
  */
 export class PersonComponent extends ItemComponent {
+    constructor(private titleService: Title,
+      protected dsoNameService: DSONameService,
+      protected localeService: LocaleService,
+      protected routeService: RouteService,
+      protected router: Router) {
+        super(dsoNameService, localeService, routeService, router);
+  }
+
+  ngOnInit(): void {
+    const lastName = this.object.firstMetadataValue('person.familyName');
+    const firstName = this.object.firstMetadataValue('person.givenName');
+    const fullName = `${firstName} ${lastName}`;
+    //set title of the page to the full name of the person
+    this.titleService.setTitle(fullName);
+    super.ngOnInit();
+  }
 }
