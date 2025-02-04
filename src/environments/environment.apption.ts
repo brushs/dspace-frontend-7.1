d39d6c00f120a7f8f1c1ce30c2787b5a07c9ec78
Steve Brush <steve.brush@apption.com>
2025-02-03 20:40:46 -0500

    Config update

--- src/environments/environment.apption.ts
+++ src/environments/environment.apption.ts
@@ -3,7 +3,7 @@ export const environment = {
 	"ui": {
 		"ssl": true,
 		"host": "dspaceangularsteve.azurewebsites.net",
-		"port": null,
+		"port": 4000,
 		"nameSpace": "/",
 		"rateLimiter": {
 			"windowMs": 60000,
