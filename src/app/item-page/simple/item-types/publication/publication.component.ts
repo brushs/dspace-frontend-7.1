24d7a5dc739896706d567424e7c4d67c4648a7f7
jjcc <jjccforth@gmail.com>
2025-10-16 08:17:27 -0400

    added photo number

--- src/app/item-page/simple/item-types/publication/publication.component.ts
+++ src/app/item-page/simple/item-types/publication/publication.component.ts
@@ -1,7 +1,14 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { ItemComponent } from '../shared/item.component';
 import { ViewMode } from '../../../../core/shared/view-mode.model';
 import { listableObjectComponent } from '../../../../shared/object-collection/shared/listable-object/listable-object.decorator';
+import { CollectionDataService } from '../../../../core/data/collection-data.service';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { DSONameService } from '../../../../core/breadcrumbs/dso-name.service';
+import { LocaleService } from '../../../../core/locale/locale.service';
+import { RouteService } from '../../../../core/services/route.service';
+import { Router } from '@angular/router';
 
 /**
  * Component that represents a publication Item page
@@ -14,8 +21,35 @@ import { listableObjectComponent } from '../../../../shared/object-collection/sh
   templateUrl: './publication.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PublicationComponent extends ItemComponent {
+export class PublicationComponent extends ItemComponent implements OnInit {
   displayLarge = false;
+  isMultimediaCollection$: Observable<boolean>;
+
+  constructor(
+    protected dsoNameService: DSONameService,
+    protected localeService: LocaleService,
+    protected routeService: RouteService,
+    protected router: Router,
+    protected collectionDataService: CollectionDataService
+  ) {
+    super(dsoNameService, localeService, routeService, router);
+  }
+
+  ngOnInit(): void {
+    super.ngOnInit();
+
+    // Check if the item belongs to the "Multimedia" collection
+    this.isMultimediaCollection$ = this.collectionDataService.findOwningCollectionFor(this.object).pipe(
+      map((collectionRD) => {
+        if (collectionRD?.hasSucceeded && collectionRD.payload) {
+          var collectionName = collectionRD.payload.name;
+          console.log('Collection Name:', collectionName);
+          return collectionName?.toLowerCase().includes('multim')
+        }
+        return false;
+      })
+    );
+  }
 
   handleShouldHide(shouldHide: boolean) {
     // shouldHide means should hide the normal file download link
