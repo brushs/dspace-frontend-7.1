142847f5c2d9f0bdffb64e33dbfcdbd361d07a93
jjcc <jjccforth@gmail.com>
2026-01-15 16:52:58 -0500

    new admin request pages for translate and publication request

---
+++ src/app/admin/admin-request-publication-page/admin-request-publication-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ds-admin-request-publication-page',
+  templateUrl: './admin-request-publication-page.component.html',
+  styleUrls: ['./admin-request-publication-page.component.scss']
+})
+export class AdminRequestPublicationPageComponent {}
---
+++ src/app/admin/admin-request-translation-page/admin-request-translation-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ds-admin-request-translation-page',
+  templateUrl: './admin-request-translation-page.component.html',
+  styleUrls: ['./admin-request-translation-page.component.scss']
+})
+export class AdminRequestTranslationPageComponent {}
